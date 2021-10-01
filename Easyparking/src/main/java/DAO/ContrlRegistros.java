70160796d20be2c7970c3f11e0e47a87fb105aaa
Lizeth Arenas <72871387+lizeth381@users.noreply.github.com>
2021-09-30 19:38:28 -0500

    Add files via upload

---
+++ Easyparking/src/main/java/DAO/ContrlRegistros.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import DAO.DAORegistro;
+import TO.TORegistro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HP
+ */
+public class ContrlRegistros {
+    DAORegistro registroDAO;
+
+    public ContrlRegistros(DAORegistro registroDAO) {
+        this.registroDAO = registroDAO;
+    }
+
+    public ContrlRegistros() {
+        this.registroDAO = registroDAO;
+    }
+    
+    public ArrayList<TORegistro> consultarRegistro() {// el consultarUsuario es el nombre de la funcionpara consultar usuarios
+        return registroDAO.consultarRegistro() ;
+        
+    }
+    public int insertarRegistros(TORegistro toregistro ) {
+        return registroDAO.insertarRegistros(toregistro );
+       }
+    
+    public boolean actualizarUsuario(TORegistro toregistro) {
+        return registroDAO.actualizarUsuario(toregistro);
+    }
+  
+    public boolean eliminarRegistro(int idRegistro) {
+        return registroDAO.eliminarRegistro(idRegistro);
+    }        
+}   
+    
+    
+    
+
